9bd583065448c10699901a4ebd2eac0e5af7dcba
Yulian Melnychuk <y.melnychuk@umt.local>
2022-07-11 12:09:35 +0200

    navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './App.css'
 
-function App() {
+import { AiFillApple,AiOutlineUser,AiFillSetting,AiFillLock,AiFillHome} from 'react-icons/ai'
+import {BsFillChatDotsFill} from 'react-icons/bs'
+import {IoHelpBuoySharp} from 'react-icons/io5'
+import {FaSignOutAlt} from 'react-icons/fa'
+import classNames from 'classnames';
+
+const App = () => {
+  const [toggle,setToggle]=useState(false)
+  console.log(toggle)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={classNames([`navigation`,toggle ?'active':''])}>
+        <ul>
+          <li>
+            <Link to="/">
+              <span className='icon'>
+               
+                <AiFillApple/>
+              </span>
+              <span className='title'>Brand Name</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span className='icon'>
+                <AiFillHome/>
+              </span>
+              <span className='title'>Dashboard</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span className='icon'>
+              <AiOutlineUser/>
+              </span>
+              <span className='title'>Customers</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span className='icon'>
+                <BsFillChatDotsFill/>
+              </span>
+              <span className='title'>Messages</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span className='icon'>
+                  <IoHelpBuoySharp/>
+              </span>
+              <span className='title'>Help</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span className='icon'>
+                  <AiFillSetting/>
+              </span>
+              <span className='title'>Settings</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span className='icon'>
+                <AiFillLock/>
+              </span>
+              <span className='title'>Passwords</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <span className='icon'>
+                  <FaSignOutAlt/>
+              </span>
+              <span className='title'>Sign Out</span>
+            </Link>
+          </li>
+        </ul>
+        <div onClick={()=>setToggle(!toggle)} className={classNames([`toggle`,toggle ?'active':''])}></div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
